a3db18297a1da2b0151997fda59f1bbb265844ce
Яна Седова <jana.sedova2809@yandex.ru>
2023-06-26 22:07:47 +0300

    редактирование ALLOWED_HOSTS

--- scrolling_text/scrolling_text/settings.py
+++ scrolling_text/scrolling_text/settings.py
@@ -1,18 +1,12 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'h6#or1-ax-62_mszd72x)^+ikxz$!59=)l2_n-o%*h_c%$+s5k'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
+ALLOWED_HOSTS = [['84.201.166.7', '127.0.0.1', 'localhost']]
 
 INSTALLED_APPS = [
     'video_maker.apps.VideoMakerConfig',
@@ -54,20 +48,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'scrolling_text.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -85,9 +71,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -98,8 +81,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
